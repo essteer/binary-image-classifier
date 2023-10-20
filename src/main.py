5d929bcd2f299b74cca858b66e910fd363d2e731
essteer <essteer@pm.me>
2023-10-20 11:48:27 +0800

    add feed forward and back propagation

--- src/main.py
+++ src/main.py
@@ -70,7 +70,8 @@ y_train = y_train[indices]
 #############################################################
 
 # Floating point equality is problematic, so use epsilon
-close_enough = lambda a, b : abs(b - a) < 1e-6
+# close_enough = lambda a, b : abs(b - a) < 1e-6  # NOTE: for scalars only
+close_enough = lambda a, b : np.linalg.norm(np.array(b - a).flatten()) < 1e-6
 
 assert x_train.shape == (N, SIDE, SIDE)
 assert y_train.shape == (N,)
@@ -165,4 +166,186 @@ assert f5 < mB < sB
 # - log(0.5) == log(2)
 assert close_enough(f5, np.log(2))
 
-print("Sanity checks succeeded")
+print("Success metric checks passed")
+
+
+#############################################################
+# Linear model - forward model
+#############################################################
+
+# ~~~~~~ Manipulating weights ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Make array of independent standard Gaussian elements
+# Scale weights by SIDE * SIDE: w.dot(x) will be at most on order of +/- sqrt(SIDE * SIDE)
+# scaling this way means decision function values will be at most on order of +/- 1
+linear_init = lambda : np.random.randn(SIDE * SIDE) / np.sqrt(SIDE * SIDE)
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Limit value range of z
+clip = lambda z : np.maximum(-15., np.minimum(+15., z))
+
+# Sigmoid function for hidden unit
+sigmoid = lambda z : 1. / (1. + np.exp(-clip(z)))
+
+
+def linear_predict(weights, input):
+    """
+    Applies sigmoid function to the dot product of
+    node weights and input
+    """
+    return sigmoid(weights.dot(input.flatten()))
+
+
+#############################################################
+# Sanity checks
+#############################################################
+
+# Initialise weights
+w = linear_init()
+
+sA = success_metrics(lambda x : linear_predict(+w, x), x_train, y_train)["acc"]
+sB = success_metrics(lambda x : linear_predict(-w, x), x_train, y_train)["acc"]
+# Probabilities should sum to 1
+assert close_enough(sA + sB, 1.)
+
+f5 = success_metrics(lambda x : linear_predict(0*w, x), x_train, y_train)["loss"]
+assert close_enough(f5, np.log(2))
+
+x = w.reshape(SIDE, SIDE)
+sA = success_metrics(lambda x : linear_predict(+w, x), [x], [DIG_A])["acc"]
+sB = success_metrics(lambda x : linear_predict(+w, x), [x], [DIG_B])["acc"]
+assert close_enough(sA, 0.)
+assert close_enough(sB, 1.)
+
+print("Forward model checks passed")
+
+#############################################################
+# Linear model - backward pass
+#############################################################
+"""
+For given x, y, get derivative (with respect to w) of
+    l(w) = loss(sigmoid(w.dot(x)), y)
+    rewritten to emphasise dependance on w, 
+                de-emphasise dependance on fixed x, y:
+    l(w) = loss_at_y(sigmoid(dot_with_x(w)))
+    where loss_at_y(p) = - log(p if y == DIG_B else 1-p)
+    where sigmoid(z) = 1/(1+exp(-z))
+    where dot_with_x(w) = w.dot(x)
+
+By __CHAIN_RULE__:
+    l'(w) = (
+          loss_at_y'(sigmoid(dot_with_x(w)))
+        * sigmoid'(dot_with_x(w))
+        * dot_with_x'(w)
+    ) = (
+          loss_at_y'(p)
+        * sigmoid'(z)
+        * dot_with_x'(w)
+    )
+    where z = dot_with_x(w)
+    where p = sigmoid(z)
+    NOTE: appearance of terms from forward pass!
+"""
+
+# NOTE: both linear_backprop_unsimp() and linear_backprop() are correct
+# the latter is purely a simplification, but both may be used
+
+def linear_backprop_unsimp(w, x, y):
+    z = w.dot(x.flatten())
+    p = sigmoid(z)
+    # Deriv. of log of X is deriv. of X, divided by X
+    dl_dp = - (+1 if y == DIG_B else -1)/(p if y == DIG_B else 1-p)
+    # p = sigmoid(z)
+    # Deriv. of sigmoid is (sigmoid * (1 - sigmoid))
+    dp_dz = p * (1 - p)
+    # Deriv. of w*x w.r.t. w = x
+    dz_dw = x.flatten()
+    
+    dl_dw = dl_dp * dp_dz * dz_dw
+    
+    return dl_dw
+
+
+def linear_backprop(w, x, y):
+    """
+    Simplifies linear_backprop_unsimplified()
+    """
+    z = w.dot(x.flatten())
+    p = sigmoid(z)
+    """
+        dl_dp = -1/p if y == DIG_B else +1/(1-p)
+        dp_dz = p * (1 - p)
+        dl_dz = dl_dp * dp_dz = - (1 - p) if y == DIG_B else +p
+    """
+    # Interpret dl_dz as residual error of p as estimator of one-hot version of y
+    dl_dz = p - (1 if y == DIG_B else 0)
+    # Deriv. of w*x w.r.t. w = x
+    dz_dw = x.flatten()
+    
+    dl_dw = dl_dz * dz_dw
+    
+    return dl_dw
+
+
+#############################################################
+# Sanity checks
+#############################################################
+
+for _ in range(10):
+    # Create test variables
+    w = linear_init()
+    x = x_train[0]
+    y = y_train[0]
+    
+    # Check that simplification preserved answer
+    g_unsimp    = linear_backprop_unsimp(w, x, y)
+    g           = linear_backprop       (w, x, y)
+    assert close_enough(g_unsimp, g)
+    
+    # Do step of gradient descent, check loss decreased
+    before = success_metrics(lambda xx: linear_predict(w, xx), [x], [y])["loss"]
+    w = w - 0.01 * g
+    after = success_metrics(lambda xx: linear_predict(w, xx), [x], [y])["loss"]
+    assert after < before
+
+print("Back propagation checks passed")
+
+
+#############################################################
+# Training loop
+#############################################################
+
+# ~~~~~~ Training parameters ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+T = 10001
+DT = 1000
+LEARNING_RATE = 0.1
+ANNEAL_T = 1000
+
+idx = 0
+
+def next_training_example():
+    global idx
+    xy = x_train[idx], y_train[idx]
+    idx += 1
+    idx %= N  # if idx == N, set to 0 so as to loop
+    return xy   
+
+
+# ~~~~~~ SGD - THE ENGINE OF LEARNING ~~~~~~~~~~~~~~~~~~~~~~~
+
+# Initialise w
+w = linear_init()
+
+for t in range(T):
+    x, y = next_training_example()
+    g = linear_backprop(w, x, y)
+    LR = LEARNING_RATE * float(ANNEAL_T) / (ANNEAL_T + t)
+    w = w - LR * g
+    
+    if t % DT : continue
+    
+    ms = success_metrics(lambda x: linear_predict(w, x), x_train, y_train)
+    print(f"at step {t:6d}; tr acc {ms['acc']:4.2f}; tr loss {ms['loss']:5.3f}")
+    
